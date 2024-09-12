db92c826704d2d92f2621c86402095556a08c288
Raul <raul.mf86@gmail.com>
2024-09-12 00:38:31 -0300

    comment

--- script/foundry/utils/upgrades/UpgradeExecutor.s.sol
+++ script/foundry/utils/upgrades/UpgradeExecutor.s.sol
@@ -21,17 +21,25 @@ import { ICreate3Deployer } from "@create3-deployer/contracts/ICreate3Deployer.s
 import { UpgradedImplHelper } from "./UpgradedImplHelper.sol";
 import { StorageLayoutChecker } from "./StorageLayoutCheck.s.sol";
 
+/**
+ * @title UpgradeExecutor
+ * @notice Script to schedule, execute, or cancel upgrades for a set of contracts
+ * @dev This script will read a deployment file and upgrade proposals file to schedule, execute, or cancel upgrades
+ */
 abstract contract UpgradeExecutor is Script, BroadcastManager, JsonDeploymentHandler, JsonBatchTxHelper {
-
-    address internal ERC6551_REGISTRY = 0x000000006551c19487814612e58FE06813775758;
     address internal CREATE3_DEPLOYER = 0x384a891dFDE8180b054f04D66379f16B7a678Ad6;
     uint256 internal CREATE3_DEFAULT_SEED = 0;
-    // PROXY 1967 IMPLEMENTATION STORAGE SLOTS
-    bytes32 internal constant IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
-    enum UpgradeModes { SCHEDULE, EXECUTE, CANCEL }
+
+    /// @notice Upgrade modes
+    enum UpgradeModes {
+        SCHEDULE, // Schedule upgrades in AccessManager
+        EXECUTE, // Execute scheduled upgrades
+        CANCEL // Cancel scheduled upgrades
+    }
+    /// @notice End result of the script
     enum Output {
-        TX_EXECUTION, // One Tx per schedule/execute
-        BATCH_TX_EXECUTION, // Use AccessManager to batch txs (multicall)
+        TX_EXECUTION, // One Tx per operation
+        BATCH_TX_EXECUTION, // Use AccessManager to batch actions in 1 tx through (multicall)
         BATCH_TX_JSON // Prepare raw bytes for multisig. Multisig may batch txs (e.g. Gnosis Safe JSON input in tx builder)
     }
 
@@ -43,11 +51,15 @@ abstract contract UpgradeExecutor is Script, BroadcastManager, JsonDeploymentHan
     ICreate3Deployer internal immutable create3Deployer;
     AccessManager internal accessManager;
 
+    /// @notice The version to upgrade from
     string fromVersion;
+    /// @notice The version to upgrade to
     string toVersion;
-
+    /// @notice action acumulator for batch txs
     bytes[] multicallData;
 
+    /// @dev check if the proxy's authority is the accessManager in the file
+    /// @param proxy The proxy address
     modifier onlyMatchingAccessManager(address proxy) {
         require(
             AccessManaged(proxy).authority() == address(accessManager),
@@ -56,27 +68,24 @@ abstract contract UpgradeExecutor is Script, BroadcastManager, JsonDeploymentHan
         _;
     }
 
+    /// @dev check if the caller has the Upgrader role
     modifier onlyUpgraderRole() {
         (bool isMember, ) = accessManager.hasRole(ProtocolAdmin.UPGRADER_ROLE, deployer);
         require(isMember, "Caller must have Upgrader role");
         _;
     }
 
-    modifier onlyScheduled(UpgradedImplHelper.UpgradeProposal memory p) {
-        (bool immediate, uint32 delay) = accessManager.canCall(
-            deployer,
-            p.proxy,
-            UUPSUpgradeable.upgradeToAndCall.selector
-        );
-        console2.log("Can call upgradeToAndCall");
-        console2.log("Immediate", immediate);
-        console2.log("Delay", delay);
-
-        require(delay > 0, "Cannot schedule upgradeToAndCall");
-        _;
-    }
-
-    constructor(string memory _fromVersion, string memory _toVersion, UpgradeModes _mode, Output _outputType) JsonDeploymentHandler("main") JsonBatchTxHelper() {
+    ///@dev Constructor
+    ///@param _fromVersion The version to upgrade from
+    ///@param _toVersion The version to upgrade to
+    ///@param _mode The upgrade mode
+    ///@param _outputType The output type
+    constructor(
+        string memory _fromVersion,
+        string memory _toVersion,
+        UpgradeModes _mode,
+        Output _outputType
+    ) JsonDeploymentHandler("main") JsonBatchTxHelper() {
         create3Deployer = ICreate3Deployer(CREATE3_DEPLOYER);
         fromVersion = _fromVersion;
         toVersion = _toVersion;
@@ -84,13 +93,13 @@ abstract contract UpgradeExecutor is Script, BroadcastManager, JsonDeploymentHan
         outputType = _outputType;
     }
 
-    /// @dev To use, run the following command (e.g. for Sepolia):
-    /// forge script script/foundry/deployment/Main.s.sol:Main --rpc-url $RPC_URL --broadcast --verify -vvvv
-
     function run() public virtual {
+        // Read deployment file for proxy addresses
         _readDeployment(fromVersion); // JsonDeploymentHandler.s.sol
+        // Load AccessManager
         accessManager = AccessManager(_readAddress("ProtocolAccessManager"));
         console2.log("accessManager", address(accessManager));
+        // Read upgrade proposals file
         _readProposalFile(fromVersion, toVersion); // JsonDeploymentHandler.s.sol
         _beginBroadcast(); // BroadcastManager.s.sol
         if (outputType == Output.BATCH_TX_JSON) {
@@ -98,37 +107,38 @@ abstract contract UpgradeExecutor is Script, BroadcastManager, JsonDeploymentHan
             deployer = multisig;
             console2.log("Generating tx json...");
         }
+        // Decide actions based on mode
         if (mode == UpgradeModes.SCHEDULE) {
             _scheduleUpgrades();
         } else if (mode == UpgradeModes.EXECUTE) {
             _executeUpgrades();
         } else if (mode == UpgradeModes.CANCEL) {
             _cancelScheduledUpgrades();
+        } else {
+            revert("Invalid mode");
         }
+        // If output is JSON, write the batch txx to file
         if (outputType == Output.BATCH_TX_JSON) {
             string memory action;
             if (mode == UpgradeModes.SCHEDULE) {
                 action = "schedule";
             } else if (mode == UpgradeModes.EXECUTE) {
                 action = "execute";
-            } else if (mode  == UpgradeModes.CANCEL) {
+            } else if (mode == UpgradeModes.CANCEL) {
                 action = "cancel";
             } else {
                 revert("Invalid mode");
             }
-            _writeBatchTxsOutput(
-                string.concat(
-                    action, "-", fromVersion, "-to-", toVersion
-                )
-            ); // JsonBatchTxHelper.s.sol
+            _writeBatchTxsOutput(string.concat(action, "-", fromVersion, "-to-", toVersion)); // JsonBatchTxHelper.s.sol
         } else if (outputType == Output.BATCH_TX_EXECUTION) {
+            // If output is BATCH_TX_EXECUTION, execute the batch txs
             _executeBatchTxs();
         }
+        // If output is TX_EXECUTION, no further action is needed
         _endBroadcast(); // BroadcastManager.s.sol
     }
 
     function _scheduleUpgrades() internal virtual;
-
 
     function _scheduleUpgrade(string memory key) internal {
         console2.log("--------------------");
@@ -140,7 +150,10 @@ abstract contract UpgradeExecutor is Script, BroadcastManager, JsonDeploymentHan
         console2.log("--------------------");
     }
 
-    function _scheduleUpgrade(string memory key, UpgradedImplHelper.UpgradeProposal memory p) private onlyMatchingAccessManager(p.proxy) onlyUpgraderRole() {
+    function _scheduleUpgrade(
+        string memory key,
+        UpgradedImplHelper.UpgradeProposal memory p
+    ) private onlyMatchingAccessManager(p.proxy) onlyUpgraderRole {
         bytes memory data = _getExecutionData(key, p);
         if (data.length == 0) {
             revert("No data to schedule");
@@ -152,7 +165,7 @@ abstract contract UpgradeExecutor is Script, BroadcastManager, JsonDeploymentHan
             (bytes32 operationId, uint32 nonce) = accessManager.schedule(
                 p.proxy, // target
                 data,
-                0// when
+                0 // when
             );
             console2.log("Scheduled", nonce);
             console2.log("OperationId");
@@ -163,11 +176,7 @@ abstract contract UpgradeExecutor is Script, BroadcastManager, JsonDeploymentHan
             console2.logBytes(multicallData[multicallData.length - 1]);
         } else if (outputType == Output.BATCH_TX_JSON) {
             console2.log("------------ WARNING: NOT TESTED ------------");
-            _writeTx(
-                address(accessManager),
-                0,
-                abi.encodeCall(AccessManager.execute, (p.proxy, data))
-            );
+            _writeTx(address(accessManager), 0, abi.encodeCall(AccessManager.execute, (p.proxy, data)));
         } else {
             revert("Unsupported mode");
         }
@@ -184,30 +193,26 @@ abstract contract UpgradeExecutor is Script, BroadcastManager, JsonDeploymentHan
         _executeUpgrade(key, p);
     }
 
-    function _executeUpgrade(string memory key, UpgradedImplHelper.UpgradeProposal memory p) private onlyMatchingAccessManager(p.proxy) {
+    function _executeUpgrade(
+        string memory key,
+        UpgradedImplHelper.UpgradeProposal memory p
+    ) private onlyMatchingAccessManager(p.proxy) {
         bytes memory data = _getExecutionData(key, p);
-        (uint48 schedule) = accessManager.getSchedule(accessManager.hashOperation(deployer, p.proxy, data));
+        uint48 schedule = accessManager.getSchedule(accessManager.hashOperation(deployer, p.proxy, data));
         console2.log("schedule", schedule);
         console2.log("Execute scheduled tx");
         console2.logBytes(data);
-    
+
         if (outputType == Output.TX_EXECUTION) {
             console2.log("Execute upgrade tx");
             // We don't currently support reinitializer calls
-            accessManager.execute(
-                p.proxy,
-                data
-            );
+            accessManager.execute(p.proxy, data);
         } else if (outputType == Output.BATCH_TX_EXECUTION) {
             console2.log("Adding execution tx to batch");
             multicallData.push(abi.encodeCall(AccessManager.execute, (p.proxy, data)));
             console2.logBytes(multicallData[multicallData.length - 1]);
         } else if (outputType == Output.BATCH_TX_JSON) {
-            _writeTx(
-                address(accessManager),
-                0,
-                abi.encodeCall(AccessManager.execute, (p.proxy, data))
-            );
+            _writeTx(address(accessManager), 0, abi.encodeCall(AccessManager.execute, (p.proxy, data)));
         } else {
             revert("Invalid output type");
         }
@@ -225,7 +230,10 @@ abstract contract UpgradeExecutor is Script, BroadcastManager, JsonDeploymentHan
         console2.log("--------------------");
     }
 
-    function _cancelScheduledUpgrade(string memory key, UpgradedImplHelper.UpgradeProposal memory p) private onlyMatchingAccessManager(p.proxy) {
+    function _cancelScheduledUpgrade(
+        string memory key,
+        UpgradedImplHelper.UpgradeProposal memory p
+    ) private onlyMatchingAccessManager(p.proxy) {
         bytes memory data = _getExecutionData(key, p);
         if (data.length == 0) {
             revert("No data to schedule");
@@ -233,7 +241,7 @@ abstract contract UpgradeExecutor is Script, BroadcastManager, JsonDeploymentHan
         if (outputType == Output.TX_EXECUTION) {
             console2.log("Execute cancelation");
             console2.logBytes(data);
-            (uint32 nonce) = accessManager.cancel(deployer, p.proxy, data);
+            uint32 nonce = accessManager.cancel(deployer, p.proxy, data);
             console2.log("Cancelled", nonce);
         } else if (outputType == Output.BATCH_TX_EXECUTION) {
             console2.log("Adding cancel tx to batch");
@@ -241,11 +249,7 @@ abstract contract UpgradeExecutor is Script, BroadcastManager, JsonDeploymentHan
             console2.logBytes(multicallData[multicallData.length - 1]);
         } else if (outputType == Output.BATCH_TX_JSON) {
             console2.log("------------ WARNING: NOT TESTED ------------");
-            _writeTx(
-                address(accessManager),
-                0,
-                abi.encodeCall(AccessManager.cancel, (deployer, p.proxy, data))
-            );
+            _writeTx(address(accessManager), 0, abi.encodeCall(AccessManager.cancel, (deployer, p.proxy, data)));
         } else {
             revert("Unsupported mode");
         }
@@ -262,18 +266,16 @@ abstract contract UpgradeExecutor is Script, BroadcastManager, JsonDeploymentHan
         }
     }
 
-    function _getExecutionData(string memory key, UpgradedImplHelper.UpgradeProposal memory p) internal returns(bytes memory data) {
+    function _getExecutionData(
+        string memory key,
+        UpgradedImplHelper.UpgradeProposal memory p
+    ) internal returns (bytes memory data) {
         if (keccak256(abi.encodePacked(key)) == keccak256(abi.encodePacked("IpRoyaltyVault"))) {
             console2.log("encoding upgradeVaults");
-            data = abi.encodeCall(
-                IVaultController.upgradeVaults, (p.newImpl)
-            );
-        } else {            
+            data = abi.encodeCall(IVaultController.upgradeVaults, (p.newImpl));
+        } else {
             console2.log("encoding upgradeUUPS");
-            data = abi.encodeCall(
-                UUPSUpgradeable.upgradeToAndCall,
-                (p.newImpl, "")
-            );
+            data = abi.encodeCall(UUPSUpgradeable.upgradeToAndCall, (p.newImpl, ""));
         }
         return data;
     }
